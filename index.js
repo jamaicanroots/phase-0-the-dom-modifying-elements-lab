b45cf8e6a214632933ccb683d906b676c4425e42
Matthew Elam <ematew418@gmail.com>
2022-08-09 16:54:20 -0400

    commit tree ba287e87b0217cb554d07f26348ed392b2da62e1

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const ul = document.querySelector('#main');
+const body = document.querySelector('body')
+ body.removeChild(ul)
+
+ const newHeader = document.createElement('h1')
+ const h1 = document.createElement('<h1>#victory')
